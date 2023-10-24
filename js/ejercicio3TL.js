67d1184a697b24958f78f6d1e1d2903abd60b50e
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-10-24 19:15:31 -0300

    Realice el ejercicio 3 de Trello.

---
+++ js/ejercicio3TL.js
@@ -0,0 +1,5 @@
+let gradosCelsius = prompt("Ingrese un valor en grados Celsius: ");
+let gradosFahrenheit = (gradosCelsius * (9/5)) + 32;
+
+console.log("Grados en celsius: " + gradosCelsius + "°C");
+console.log("Grados en fahrenheit: " + gradosFahrenheit + "°F");
