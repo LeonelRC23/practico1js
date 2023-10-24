8d0aceb0f11e7bab94774d0816312c2cf18d6d13
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-10-24 18:56:41 -0300

    Realice el ejercicio 2 de Trello.

---
+++ js/ejercicio2TL.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese un nombre: ");
+let apellido = prompt("Ingrese un apellido: ");
+let nombreCompleto = "El nombre resultante es: " + nombre + " " + apellido;
+
+console.log(nombreCompleto);
